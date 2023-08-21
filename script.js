bd48c6018f37a7a9b3777e4bfa499a468aef1f53
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-08-21 18:36:15 +0700

    Add DOM manipulations for project card

---
+++ script.js
@@ -0,0 +1,23 @@
+const projectContainer = document.querySelector('.project-container');
+
+const projectCard = document.createElement('div');
+projectCard.classList.add('project-cards');
+
+const projectText = document.createElement('div');
+projectText.classList.add('project-text');
+projectCard.appendChild(projectText);
+
+const projectTitle = document.createElement('h4');
+projectText.appendChild(projectTitle);
+
+const projectStart = document.createElement('p');
+projectText.appendChild(projectStart);
+
+const projectFinish = document.createElement('p');
+projectText.appendChild(projectFinish);
+
+const projectRating = document.createElement('p');
+projectText.appendChild(projectRating);
+
+const projectButtons = document.querySelector('.project-buttons');
+projectCard.appendChild(projectButtons);
