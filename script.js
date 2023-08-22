c71b0f47fd2943edd6387125479b40b70c768ab4
s0ycloud <135024553+s0ycloud@users.noreply.github.com>
2023-08-22 14:48:20 +0700

    Add the ability to add texts (title, start/end date) to card

--- script.js
+++ script.js
@@ -18,35 +18,47 @@ function newProjectCard() {
 
     const projectTitle = document.createElement('h4');
     projectText.appendChild(projectTitle);
-    let title = prompt("What is the title of the game?");
-    projectTitle.textContent = `${title}`;
+    projectCardTitle(projectTitle);
 
     const projectStart = document.createElement('p');
     projectText.appendChild(projectStart);
-    let projectDateStart = prompt("When did you start the game? Please make sure it is on DD/MM/YYYY format.");
-    dateCheckError(projectDateStart);
-    projectStart.innerHTML = `<strong>Started:</strong> ${projectDateStart}`;
+    projectCardStartDate(projectStart);
 
     const projectFinish = document.createElement('p');
     projectText.appendChild(projectFinish);
-    let projectDateEnd = prompt("When did you finish the game? Please make sure it is on DD/MM/YYYY format.");
-    dateCheckError(projectDateEnd);
-    projectFinish.innerHTML = `<strong>Finished:</strong> ${projectDateEnd}`;
+    projectCardEndDate(projectFinish);
 
     const projectRating = document.createElement('p');
     projectText.appendChild(projectRating);
 };
 
 function dateCheckError(datePrompt) {
-    if (datePrompt === undefined) return;
-    datePrompt.split('/').forEach((num, i, arr) => {
-        if (arr[1] > 12) {
-            datePrompt = prompt("Wrong format! Make sure to enter the date like this DD/MM/YYYY.");
-        };
-    });
-    if (datePrompt.length !== 10) {
-        while (datePrompt.length !== 10) {
-            datePrompt = prompt("Wrong format! Make sure to enter the date like this DD/MM/YYYY.");
-        };
+    if (datePrompt === '') return "N/A";
+    while (datePrompt.match(/^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|(([1][26]|[2468][048]|[3579][26])00))))$/g) === null) {
+        datePrompt = prompt("Wrong format! Make sure to enter the date like this DD/MM/YYYY.");
     };
+    return datePrompt;
+};
+
+function projectCardTitle(node) {
+    let title = prompt("What is the title of the game?");
+    while (title === '') {
+        title = prompt("What is the title of the game?");
+    };
+    if (title === null) return;
+    node.textContent = `${title}`;
+};
+
+function projectCardStartDate(node) {
+    let date = prompt("When did you start this game? Please make sure to use DD/MM/YYYY format. If you haven't started the game, leave it blank.");
+    if (date === null) return;
+    date = dateCheckError(date);
+    node.innerHTML = `<strong>Started:</strong> ${date}`;
+};
+
+function projectCardEndDate(node) {
+    let date = prompt("When did you finish this game? Please make sure to use DD/MM/YYYY format. If you haven't started the game, leave it blank.");
+    if (date === null) return;
+    date = dateCheckError(date);
+    node.innerHTML = `<strong>Finished:</strong> ${date}`;
 };
